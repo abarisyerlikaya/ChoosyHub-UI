6553f1f61618aad6234e373ccb475b034eab91a9
Barış Yerlikaya <63058972+abarisyerlikaya@users.noreply.github.com>
2021-01-06 18:58:46 +0300

    Add files via upload

--- src/components/utils/Navi.js
+++ src/components/utils/Navi.js
@@ -1,33 +1,10 @@
 import React from "react";
-import { Button, UncontrolledCollapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Input } from "reactstrap";
-import { useHistory } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Navbar, NavbarBrand } from "reactstrap";
 
 export default function Navi() {
-  const history = useHistory();
-
-  const params = new URLSearchParams();
-
-  const handleInputChange = (event) => params.set("key", event.target.value);
-  const redirectToResults = () => history.push("/search-results" + (params ? "?" + params.toString() : ""));
-
   return (
     <Navbar color="dark" dark expand="md" className="text-light sticky-top">
       <NavbarBrand href="/">ChoosyHub</NavbarBrand>
-      <NavbarToggler id="navbar-toggler" />
-      <UncontrolledCollapse navbar toggler="#navbar-toggler">
-        <Nav className="ml-auto" navbar>
-          <NavItem className="py-1 px-1">
-            <Input size="sm" className="rounded-pill" placeholder="Ürün ara..." onChange={handleInputChange} />
-          </NavItem>
-          <NavItem className="py-1 px-1">
-            <Button outline color="light" size="sm" className="rounded-pill" onClick={redirectToResults}>
-              <FontAwesomeIcon icon={faSearch} />
-            </Button>
-          </NavItem>
-        </Nav>
-      </UncontrolledCollapse>
     </Navbar>
   );
 }
